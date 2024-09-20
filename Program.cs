f2a76204cde51b72bb560e6d627ae32e56aa7d6d
Daniel73636 <danielmontiel1600@gmail.com>
2024-09-20 14:50:30 -0500

    Cords

--- Program.cs
+++ Program.cs
@@ -18,17 +18,23 @@ builder.Services.AddScoped<IUsers, UsersServices>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Cords", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
+app.UseCors("Cords");
 app.MapControllers();
 
 app.Run();
